4c38b9563da4c5807b58a3b752dd2576b79e59d8
Raphael <raphaelrodrigues93@gmail.com>
2022-10-21 15:24:14 -0300

    Corrigindo escrita do nome do pokemon

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <section>
+          <div>
+            <div
+              v-for="pokemon in pokemons.slice(0, 50)" 
+              :key="pokemon.name"
+            >
+              <div>
+                <img 
+                  :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${get_id(pokemon)}.svg`" 
+                  :alt="pokemon.name"
+                >
+                <h2>{{get_name(pokemon)}}</h2>
+              </div>
+            </div>
+          </div>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  components: {},
+
+  data: () => {
+    return{
+      pokemons: []
+    }
+  },
+
+  mounted () {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=100').then((response) => {
+      this.pokemons = response.data.results;
+    })
+  },
+  methods: {
+    get_id(pokemon) {
+      return Number(pokemon.url.split('/')[6])
+    },
+   get_name(pokemon) {
+    return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+   } 
   }
 }
 </script>
